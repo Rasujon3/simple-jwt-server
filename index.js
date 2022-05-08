407d926316ee130d6550a0fd60dd7937a1aaf099
Rasujon3 <rasujon3@gmail.com>
2022-05-08 22:11:34 +0600

    70_5-2 Simple jwt token project and api setup

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
 const port = process.env.PORT || 5000;
 
 const app = express();
@@ -9,11 +9,22 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("Hello from simple JWT Server");
+});
 
-app.get('/', (req, res) =>{
-    res.send('Hello from simple JWT Server')
-})
+app.post("/login", (req, res) => {
+  const user = req.body;
+  console.log(user);
+  //   DANGER: Do not check password here for serious application
+  //   Use proper process for hashing and checking
+  //   After completing all authenticatin related verification, issue JWT token
+  if (user.password === "123456") {
+  } else {
+  }
+  res.send({ success: true });
+});
 
 app.listen(port, () => {
-    console.log('Listening to port', port);
-})
+  console.log("Listening to port", port);
+});
